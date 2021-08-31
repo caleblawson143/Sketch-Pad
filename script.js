a05e0d9eb1bb82e6d0a0e934f842580eb3ff5d0d
Caleb <calebmlawson@gmail.com>
2021-08-31 14:34:31 -0500

    Final commit
    Added clear button to clear the sketch pad
    added a color selector
    added a grid size changer

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-const gridWrapper = document.querySelector('.wrapper');
+const gridWrapper = document.querySelector('.grid-wrapper');
+const clearBtn = document.querySelector('.clear-btn');
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.slider-value');
+const colorPicker = document.querySelector('.color-picker');
 
-for (i = 0; i < 256; i++) {
-    gridWrapper.appendChild("div");
+let chosenColor = colorPicker.value;
+let gridBoxesArray = [];
+
+// Creating divs and adding hover effects
+function createGrid() {
+for (i = 0; i < slider.value * slider.value; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridBox');
+        gridItem.addEventListener('mouseenter', function(e) {
+            e.target.style.backgroundColor = chosenColor;
+            gridItem.classList.add('gridBoxOnHover');
+        })
+        // This is just a cool mouse tracking function I accidently made
+        /*gridItem.addEventListener('mouseleave', function(e) {
+            e.target.style.backgroundColor = 'white';
+            gridItem.classList.remove('gridBoxOnHover');
+     })*/
+        gridWrapper.appendChild(gridItem);
+        gridBoxesArray.push(gridItem);
+    }
 }
+
+// Clear button function
+function clearSketch() {
+    gridBoxesArray.forEach((div) => {
+        div.style.backgroundColor = 'white'
+        div.classList.remove('gridBoxOnHover');
+    })
+}
+
+function changeColor() {
+    chosenColor = colorPicker.value;
+}
+
+slider.addEventListener('input', function() {
+    sliderValue.textContent = "Grid Size: " + slider.value;
+    gridWrapper.style = 'grid-template-rows: repeat(' + slider.value + ' , 1fr); grid-template-columns: repeat(' + slider.value + ', 1fr);';
+    gridBoxesArray.forEach((div) => {
+        div.remove();
+    })
+    createGrid();
+})
+
+clearBtn.addEventListener('click', clearSketch);
+colorPicker.addEventListener('input', changeColor);
+window.onload = (createGrid);
